eb3ca29a39a6239a0db783e60003ca7858489d27
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2025-06-17 13:55:54 +0300

    Update DBHelper.java

--- task_manager/src/task_manager/DBHelper.java
+++ task_manager/src/task_manager/DBHelper.java
@@ -7,7 +7,7 @@ import java .sql.SQLException;
 public class DBHelper {
 	private static final String URL="jdbc:mysql://localhost:3306/tasks";
 	private static final String USER="root";
-	private static final String PASS="xm180605";
+	private static final String PASS="";//enter your own password
 	
 	public static Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(URL,USER,PASS);
